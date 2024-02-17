f133b3f836a609b1bf12c128ea0e3449bf8e519d
Anmol Arora <heymailme04@gmail.com>
2024-02-17 10:46:17 +0530

    Added view page rendering/request

--- app.js
+++ app.js
@@ -21,15 +21,18 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-});
-
-app.get('/makecampground', async (req,res) => {
-    const camp = new Campground({title: 'ExtraVegan', price: '$121'});
-    await camp.save()
-    res.send(camp)
 })
 
+app.get('/campgrounds', async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds})
+})
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
+})
 
 app.listen(3000, () => {
     console.log('Running on port 3000');
-});
+})
