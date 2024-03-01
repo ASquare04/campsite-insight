351aa07a27424bf41086716ea18d0e0f500e7edd
Anmol Arora <heymailme04@gmail.com>
2024-03-01 15:47:55 +0530

    Adding on session, flash-mesaages

--- routes/review.js
+++ routes/review.js
@@ -10,6 +10,7 @@ router.post('/', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
+    req.flash('success', 'Added a new Review!')
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -19,6 +20,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Deleted the Review!')
     res.redirect(`/campgrounds/${id}`);
 }))
 
