172510f68c6b0463af83ca5fc56f683365a3619d
Anmol Arora <heymailme04@gmail.com>
2024-03-09 11:31:47 +0530

    Updated the `images`

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities')
-const {places, descriptors, descText} = require('./seedHelpers')
+const {places, descriptors, descText, image} = require('./seedHelpers')
 const Campground = require('../models/campground')
 
 mongoose.connect('mongodb://localhost:27017/camp-insight', {
@@ -17,13 +17,13 @@ db.once("open", () => {
 const sample = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for(let i=0; i< 20; i++){
+    for(let i=0; i< 15; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 15) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:'https://unsplash.com/collections/483251',
+            image:`${sample(image)}`,
             description: sample(descText),
             price
         })
