ee337a99f8012a44581d303ceb676d3eed7848d5
Anmol Arora <heymailme04@gmail.com>
2024-03-18 16:26:55 +0530

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('Running')
+const target = document.getElementById('target');
+
+function updatePosition(event) {
+  const mouseX = event.clientX;
+  const newPosition = mouseX / window.innerWidth * 100;
+  target.style.left = newPosition + '%';
+}
+
+document.addEventListener('mousemove', updatePosition);
