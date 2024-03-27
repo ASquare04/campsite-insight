be44e7b3e622f4c47a26add90036610d4fb57c5b
Anmol Arora <heymailme04@gmail.com>
2024-03-27 19:11:44 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const ExpressError = require('./utils/ExpressError');
+const Campground = require('./models/campground');
+const Joi  = require('joi')
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl
@@ -5,4 +9,34 @@ module.exports.isLoggedIn = (req, res, next) => {
         return res.redirect('/login');
     }
     next();
+}
+
+
+module.exports.validateCamps = (req,res,next) =>{
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            location: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    });    
+    const {error} = campgroundSchema.validate(req.body)
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async(req, res, next) =>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)){
+        req.flash('error', 'Oops!You do not have such grant')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    next()
 }
