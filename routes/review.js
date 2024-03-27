f8ad9a7caa4f9d8d1976b329e11da9165acb785c
Anmol Arora <heymailme04@gmail.com>
2024-03-27 19:08:46 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const Joi  = require('joi')
+const { isLoggedIn } = require('../middleware')
 
-router.post('/', catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
-    req.flash('success', 'Added a new Review!')
     await review.save();
     await campground.save();
+    req.flash('success', 'Added a new Review!')
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
