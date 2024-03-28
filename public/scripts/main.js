340e10ca03b3fc12a6998f9d237d55774f225607
Anmol Arora <heymailme04@gmail.com>
2024-03-28 20:08:40 +0530

    GSAP

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,1 @@
-const target = document.getElementById('target');
-
-function updatePosition(event) {
-  const mouseX = event.clientX;
-  const newPosition = mouseX / window.innerWidth * 100;
-  target.style.left = newPosition + '%';
-}
-
-document.addEventListener('mousemove', updatePosition);
+gsap.to('#bear',{})
